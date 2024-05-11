becb36446156e08aeac690b957007dd0156e7b49
aidoha <ibrayevaiqyn@gmail.com>
2024-05-11 12:34:50 +0500

    fix/deploy

--- index.js
+++ index.js
@@ -2,17 +2,24 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const usersRouter = require('./routes/users');
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-
-mongoose.connect('mongodb+srv://aiqyyn1:aikyn777@user.4k1gzv6.mongodb.net/?retryWrites=true&w=majority&appName=user', { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
-db.on('error', (error) => console.error(error));
-db.once('open', () => console.log('Connected to Database'));
-
-const usersRouter = require('./routes/users');
 app.use('/users', usersRouter);
+const uri =
+  'mongodb+srv://aiqyyn1:aikyn777@user.4k1gzv6.mongodb.net/?retryWrites=true&w=majority&appName=user';
+const start = async () => {
+  try {
+    await mongoose.connect(uri);
+    console.log('Connected to MongoDB');
 
-app.listen(3001, () => console.log('Server Started'));
+    app.listen(3001, () => {
+      console.log(`Server is running on http://localhost:3001`);
+    });
+  } catch (e) {
+    console.error(e);
+  }
+};
+start();
